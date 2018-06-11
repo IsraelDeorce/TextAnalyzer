0b4aacc4dbc804779d193fdd25279049aa987c78
IsraelDeorce <israel.idvj@gmail.com>
2018-06-11 17:32:15 -0300

    Using JavaFX

--- TextAnalyser/src/main/App.java
+++ TextAnalyser/src/main/App.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -7,38 +8,93 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.stream.Stream;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * It is necessary to convert any file to .txt format for this application to
  * work http://pdftotext.com/pt/
  * 
- * @author Israel Deorce
- * @date/06/2019
+ * @author Israel Deorce @date/06/2019
  *
  */
-public class App {
+public class App extends Application {
+	private Button btnFileChooser;
+	private Button btnRun;
 
-	// How many files contains the sentence
-	public static int nOccurences = 0;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-	public static void main(String[] args) {
+		BorderPane pane = new BorderPane();
+		pane.setStyle("-fx-background-color: #008774;");
 
-		 String folderPath = args[0];
-		 String ref = args[1];
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(15, 12, 15, 12));
+		hbox.setSpacing(10);
 
-		//String folderPath = ("C:/Users/Israel/Desktop/folder");
-		//String ref = ("Pair Programming Illuminated");
+		btnFileChooser = new Button("Click to Choose a Folder");
+		btnFileChooser.setPrefSize(150, 20);
+		btnFileChooser.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				DirectoryChooser directoryChooser = new DirectoryChooser();
+				File selectedDirectory = directoryChooser.showDialog(primaryStage);
+				if (selectedDirectory != null) {
+					String folderPath = selectedDirectory.getAbsolutePath();
+					String ref = ("Pair Programming Illuminated");
+					String[] splited = ref.split("\\s+");
+					getArticlesArticle(folderPath, ref, splited);
+				} else {
+				}
+			}
+		});
 
+		Label label = new Label("Type a String");
+		label.setTextFill(Color.web("#FFFFFF"));
+
+		TextField notification = new TextField();
+
+		btnRun = new Button("Run!");
+		btnRun.setPrefSize(50, 20);
+
+		hbox.getChildren().addAll(btnFileChooser, label, notification, btnRun);
+
+		pane.setTop(hbox);
+
+		Scene scene = new Scene(pane, 500, 500);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Mapas com JavaFX");
+		primaryStage.show();
+
+		String folderPath = ("C:/Users/Israel/Desktop/folder");
+		String ref = ("Pair Programming Illuminated");
 		String[] splited = ref.split("\\s+");
-
 		// For checking if the splited version of the reference is correct
 		// for(int i=0; i<splited.length;i++) {
 		// System.out.println(splited[i]);
 		// }
-
-		findReferencesByArticle(folderPath, ref, splited);
+		// findReferencesByArticle(folderPath, ref, splited);
 
 	}
+
+	// ##########################################################################################
+
+	// How many files contains the sentence
+	public static int nOccurences = 0;
 
 	/**
 	 * Method that gets every file from a given folder and send its path to the
@@ -48,7 +104,7 @@ public class App {
 	 * @param ref
 	 * @param splited
 	 */
-	public static void findReferencesByArticle(String folderPath, String ref, String[] splited) {
+	public static void getArticlesArticle(String folderPath, String ref, String[] splited) {
 		System.out.println(folderPath);
 		try (Stream<Path> paths = Files.walk(Paths.get(folderPath))) {
 			paths.filter(Files::isRegularFile).forEach(p -> streamService(p, ref, splited));
@@ -94,5 +150,9 @@ public class App {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
